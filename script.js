bf88407a7e179c84e8b57e0f065c7801abe58a91
Craig Gant <craigkgant@gmail.com>
2019-12-09 15:02:01 -0500

    integrated inital thoughts/pseudocode from Jorge

---
+++ script.js
@@ -0,0 +1,23 @@
+// create event when save buttton is clcked
+// IMPORTANT-- when you click the button, you're getting the value of a different element. So make sure that they are (somehow) linked together.
+
+
+
+//display the items from localStorage to the DOM
+// for loop?
+
+for (let i=9; i<18; i++) {
+    
+}
+
+
+// change the color of the 'time block section' depending of the the current time
+//use an each statement
+
+
+
+
+//display the current day and time to DOM
+//import the cdn and put in html. use moment.js (should already be at the bottom)
+//(Read the documentation on moment.js site to know how to reference it)
+// $("#current-day").text(moment())...etc
