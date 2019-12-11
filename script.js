e4008d64191f9883d2464091c09ece6fb5dd16c8
Craig Gant <craigkgant@gmail.com>
2019-12-10 22:47:25 -0500

    Added dynamic color changes to calendar based on the current time

--- script.js
+++ script.js
@@ -1,23 +1,56 @@
+//Empty array for things to do:
+let thingsToDo = [];
 // create event when save buttton is clcked
 // IMPORTANT-- when you click the button, you're getting the value of a different element. So make sure that they are (somehow) linked together.
 
+populate();
+
+$(".saveBtn").on("click", function(){
+    event.preventDefault();
+    //take value from textarea $(this).prev().val() and save to local storage
+    let oneThing = $(this).prev().val().trim();
+    //adds the value of this textarea to the array thingsToDo
+    thingsToDo.push(oneThing);
+    //adds the array to storage
+    localStorage.setItem("toDos", JSON.stringify(thingsToDo));
+});
 
 
 //display the items from localStorage to the DOM
 // for loop?
+function populate() {
+    //empty out text in textareas
+    $(".textarea").empty();
+    // localStorage.getItem(JSON.parse(thingsToDo)); HOW TO GET THINGS OUT??
 
-for (let i=9; i<18; i++) {
-    
+    for (let i = 0; i < thingsToDo.length; i++)  {
+    let textItem = thingsToDo[i];
+    data-val[i].textContent(textItem);
+    console.log(data-val[i]);
+    }
 }
-
-
 // change the color of the 'time block section' depending of the the current time
 //use an each statement
 
+    $(".textarea").each(function(j, item) {
+    let calendarTime = $(this).attr("data-val");
+    //if moment (downgraded to hour) = inex, then make color red
+    if (moment().startOf().format("HH") === calendarTime) {
+        //change color to red (.present)
+        $(this).addClass("present");
+    }
+    //if moment (downgraded to hour) = inex, then make color red
+    else if (moment().startOf().format("HH") < calendarTime) {
+        //change color to green (.future)
+        $(this).addClass("future");
+    }
+    else {
+        //color is normal/white (.past)
+        $(this).addClass("past");
+    }
+})
 
 
 
 //display the current day and time to DOM
-//import the cdn and put in html. use moment.js (should already be at the bottom)
-//(Read the documentation on moment.js site to know how to reference it)
-// $("#current-day").text(moment())...etc
+$("#currentDay").text(moment().format('LLLL'));
